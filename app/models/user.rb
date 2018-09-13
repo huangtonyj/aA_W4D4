f298e147091e8234a6eeee87469b5a5196ce0008
tonyjhuang <huangtonyj91@gmail.com>
2018-09-13 11:33:50 -0700

    Complete User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,38 @@
 class User < ApplicationRecord
+
+  validates :email, :password_digest, :session_token, presence: true
+  validates :password, length {minimum: 6, allow_nil: true}
+  validates :email, :session_token, uniqueness: true
+
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    session[:session_token] ||= User.generate_session_token
+  end
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def self.find_by_credentials(email, pw)
+    user = User.find_by(email: email)
+    is_password?(pw) ? user : nil
+    # user.is_password?(pw) ? user : nil
+  end
+
 end
